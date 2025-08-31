e76bf92cc4b16625c529ba8f35ec811fd6b21db8
marvinobig <88102362+marvinobig@users.noreply.github.com>
2025-08-31 16:37:47 +0200

    create auth views for login, logout and registration

---
+++ auth/views.py
@@ -0,0 +1,57 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
+from .forms import RegisterForm
+
+# Create your views here.
+def registerView(request):
+    if request.method == "POST":
+        form = RegisterForm(request.POST)
+
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+
+            user = User.objects.create_user(username=username, password=password)
+            login(request, user)
+
+            return redirect("dashboard")
+        else:
+            form = RegisterForm()
+            context = {"form", form}
+
+            return render(request, "register.html", context)
+    else:
+        form = RegisterForm(request.POST)
+        context = {"form":form}
+        return render(request, 'register.html', context)
+        
+
+def loginView(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            nextUrl = request.POST.get('next') or request.GET.get('next') or 'dashboard'
+
+            return redirect(nextUrl)
+        else:
+            errMsg = "Invalid credentials"
+            context = { "err": errMsg}
+
+            return render(request, 'login.html', context)
+    else:
+        return render(request, 'login.html')
+
+
+def logoutView(request):
+    if request.method == "POST":
+        logout(request)
+
+        return redirect('login')
+    else:
+        return render(request, "logout.html")
