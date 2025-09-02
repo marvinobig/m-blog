8d74e8c96ef7d87aef4df537f2e9337f9d0e9d16
marvinobig <88102362+marvinobig@users.noreply.github.com>
2025-09-02 13:32:32 +0200

    add some validation logic for post form

--- markdownPosts/forms.py
+++ markdownPosts/forms.py
@@ -4,5 +4,13 @@ class CreatePostForm(forms.Form):
     title = forms.CharField(max_length=100)
     body = forms.CharField(widget=forms.Textarea)
 
-    def publish(self):
-        pass 
+    def clean(self):
+        cleanedData = super().clean()
+        title = cleanedData.get('title')
+        body = cleanedData.get('body')
+
+        if len(title) < 5:
+            raise forms.ValidationError("Title is too short. It sould be greater than 5 characters")
+        
+        if len(body) < 20:
+            raise forms.ValidationError("Body does not have enough content. It should be greater than 20 characters")
