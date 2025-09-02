873a6fe98ef7a0a1398d4e6447aa20e32b3229ff
marvinobig <88102362+marvinobig@users.noreply.github.com>
2025-09-02 14:26:18 +0200

    expand dashboard view
    also make use of login_required() decorator

--- markdownPosts/views.py
+++ markdownPosts/views.py
@@ -1,11 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+from .models import Post
 
 # Create your views here.
-def dashboardView(request):
-    pass
+@login_required
+def dashboardView(request, userId):
+    user = get_object_or_404(User, id=userId)
+    userPosts = user.posts.all()
+    context = {"user": user, "userPosts": userPosts}
 
+    return render(request, "dashboard.html", context)
+
+@login_required
 def allPostsView(request):
+    posts = Post.objects.all()
     pass
 
+@login_required
 def createPostView(request):
     pass
